4e2c2af8bdb6ccffab56baf0327d80a4c8c20d9f
Shresth55 <81299919+Shresth55@users.noreply.github.com>
2021-06-11 18:49:30 +0530

    Add files via upload

---
+++ bird.js
@@ -0,0 +1,34 @@
+class Bird{
+    constructor(x,y){
+        var option={
+            'restitution':0.8,
+            'friction':0.3,
+            'density':1.0
+    
+        }
+    this.body= Bodies.rectangle(x,y,50,50,option)
+    World.add (world,this.body);
+    this.width =50
+    this.height =50
+    
+    }
+    display(){
+        var pos =this.body.position;
+        pos.x = mouseX;
+        pos.y = mouseY;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+       stroke("red");
+       strokeWeight(4);
+        rectMode(CENTER);
+        fill("blue");
+        rect(0, 0, this.width, this.height);
+        pop();
+      }
+    
+    
+    
+    
+    }
